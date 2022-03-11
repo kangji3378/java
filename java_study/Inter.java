e7c0c43270254762d30ba8af39e8bc19d02feb27
Jiseung Kang <kangji3378@gmail.com>
2022-03-11 22:23:59 +0900

    3-day

--- java_study/Inter.java
+++ java_study/Inter.java
@@ -8,10 +8,12 @@ interface Predator{
     default void printFood() {
         System.out.printf("my food is %s\n", getFood());
     }
-    /*디폴트 메서드는 메소드명 가장 앞에 "default" 라고 표기해야 한다. 
-    이렇게 Predator 인터페이스에 printFood 디폴트 메서드를 구현하면 
-    Predator 인터페이스를 구현한 Tiger, Lion 등의 실제 클래스는 printFood 메서드를 구현하지 않아도 사용할 수 있다. 
-    그리고 디폴트 메스드는 오버라이딩이 가능하다. 즉, printFood 메서드를 실제 클래스에서 다르게 구현하여 사용할수 있다. */
+    /*디폴트 메서드
+        인터페이스의 메서드는 몸통(구현체)을 가질 수 없지만 디폴트 메서드를 사용하면 실제 구현된 형태의 메서드를 가질 수 있다.
+        디폴트 메소드는 메소드명 가장 앞에 "default" 라고 표기해야 한다. 
+        이렇게 Predator 인터페이스에 printFood 디폴트 메서드를 구현하면 
+        Predator 인터페이스를 구현한 Tiger, Lion 등의 실제 클래스는 printFood 메서드를 구현하지 않아도 사용할 수 있다. 
+        그리고 디폴트 메스드는 오버라이딩이 가능하다. 즉, printFood 메서드를 실제 클래스에서 다르게 구현하여 사용할수 있다. */
 }
 
 
